6bd372e36eac79b9ab313104fd095c7d4bf9394a
ftgr <fernandestgr@gmail.com>
2024-02-29 12:08:38 -0400

    Print Sum of Values Divisible by 3 AND 5 - Trivial & Optimized Solutions - Solution

---
+++ Section 17/Print Sum of Values Divisible by 3 AND 5 - Trivial & Optimized Solutions - Quest.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int main()
+{
+    int number, i, sum, sum2;
+    printf("Enter an endpoint number: ");
+    scanf("%d", &number);
+
+    sum = 0;
+    sum2 = 0;
+
+    for (i = 1; i <= number; i++)
+    {
+        if (i % 3 == 0 && i % 5 == 0)
+        { 
+            sum = sum + i;
+        }
+        
+    }
+    printf("Sum: %d\n", sum);
+
+       for (i = 15; i <= number; i  = i + 15)
+    {
+        printf("%d can be divided by both 3 and 5\n", i);
+       sum2 = sum2 + i;  
+    }
+    printf("Optmized solution\nSum = %d\n", sum2);
+
+    return 0;
+}
