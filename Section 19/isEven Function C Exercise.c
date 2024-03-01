987bc1a68c5e8616b38d258e83bcc7958d326566
ftgr <fernandestgr@gmail.com>
2024-03-01 10:56:02 -0400

    "isEven" Function C Exercise - Solution

---
+++ Section 19/isEven Function C Exercise.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int isEven(int num)
+{
+    if (num % 2 == 0)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+int main()
+{
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    if (isEven(num) == 1)
+    {
+        printf("%d is an even number\n", num);
+    }
+    else
+    {
+        printf("%d is an odd number\n", num);
+    }
+    return 0;
+}
