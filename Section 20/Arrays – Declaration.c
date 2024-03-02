f2c4b12f3b2f32386e9c8400b15bc122b7172c81
ftgr <fernandestgr@gmail.com>
2024-03-02 10:58:03 -0400

    Arrays – Declaration

---
+++ Section 20/Arrays – Declaration.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int grade[5]; // an array with 5 elements of type integer  
+
+    double salaries[10]; // an array with 10 elements of type double
+
+    char myArray[100]; // an array with 100 elements of type char
+
+    return 0;
+}
