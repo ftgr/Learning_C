80b3dd5061001794a620e43acf023b4a1f517eee
ftgr <fernandestgr@gmail.com>
2024-03-02 11:07:10 -0400

    Arrays - Initialization

---
+++ Section 20/Arrays - Initialization.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int arr[3] = {5, 7, 10}; // an array with 3 elements of type integer
+
+    float bArra[5] = {1.1, 2.2, 3.3, 4.4, 5.5}; // an array with 5 elements of type float
+
+    char name[4] = {'J', 'o', 'h', 'n'}; // an array with 4 elements of type char
+
+    int zeroArray[5] = {0}; // an array with 5 elements of type integer and initialized to 0
+
+    int partialArray[5] = {1, 2, 3}; // an array with 5 elements of type integer and with the first 3 elements initialized to 1, 2, 3, and the remaining elements initialized to 0
+
+    return 0;
+}
