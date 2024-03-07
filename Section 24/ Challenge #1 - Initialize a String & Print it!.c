baa37799adabdcca104c012e1da8a735349105c8
ftgr <fernandestgr@gmail.com>
2024-03-07 10:10:17 -0400

    Fix formating error

--- Section 24/ Challenge #1 - Initialize a String & Print it!.c
+++ Section 24/ Challenge #1 - Initialize a String & Print it!.c
@@ -5,13 +5,12 @@ int main()
     printf("%s\n", fullName);
 
     int i = 0;
-
     while (fullName[i] != '\0')
     {
         printf("%c", fullName[i]);
         i++;
     }
+
     printf("\n");
-    
     return 0;
 }
