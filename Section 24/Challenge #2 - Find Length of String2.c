047b26b9bd726dc292104bef2e5a70084da03002
ftgr <fernandestgr@gmail.com>
2024-03-07 11:17:44 -0400

    Alternative Solution

--- Section 24/Challenge #2 - Find Length of String2.c
+++ Section 24/Challenge #2 - Find Length of String2.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
+#include <string.h> // so strlen() doesn't complain!
 
 int stringLength(char *str);
+
+int stringLenghtAlternative(char *str);
 
 int main()
 {
     char name[30];
     int size;
+    int size2;
 
     printf("Enter your name: ");
     fgets(name, sizeof(name), stdin);
@@ -13,9 +17,23 @@ int main()
     size = stringLength(name);
     printf("Your name has %d characters.\n", size);
 
+    size2 = stringLenghtAlternative(name);
+    printf("Your name has %d characters.\n", size2);
+
     return 0;
 }
 
-int stringLength(char *str){
+int stringLength(char *str)
+{
     return strlen(str);
+}
+
+int stringLenghtAlternative(char *str)
+{
+    int i = 0;
+    while (str[i] != '\0')
+    {
+        i++;
+    }
+    return i;
 }
