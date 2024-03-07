5697b8c32d5ed679fe212780f0824ba4beec84ff
ftgr <fernandestgr@gmail.com>
2024-03-07 10:07:23 -0400

    Challenge #1 - Solution

---
+++ Section 24/ Challenge #1 - Initialize a String & Print it!.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    char fullName[] = "Bill Clinton";
+    printf("%s\n", fullName);
+
+    int i = 0;
+
+    while (fullName[i] != '\0')
+    {
+        printf("%c", fullName[i]);
+        i++;
+    }
+    printf("\n");
+    
+    return 0;
+}
